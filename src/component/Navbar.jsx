7b32fee5bd30cdcbb6f35326256c16dcd33449fc
ratul-hasan00 <afif.ratul00@gmail.com>
2025-09-27 22:19:18 +0600

    second commit-added navbar and progressresolved

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center max-w-[1250px] mx-auto mt-3 pb-2 shadow-xs px-2'>
+          <div>
+              <h1 className='text-2xl font-bold'>CS — Ticket System</h1>
+          </div>
+
+        <div className='flex items-center gap-4'>
+            <ul className='flex gap-4'>
+                <li>Home</li>
+                <li>FAQ</li>
+                <li>Changelog</li>
+                <li>Blog</li>
+                <li>Download</li>
+                <li>Contact</li>
+            </ul>
+            <button className='px-4 py-2 bg-gradient-to-r from-[#632ee3] to-[#9f62f2] text-white'>+ New Ticket</button>
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
---
+++ src/component/ProgressResolved.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import vector_1 from '../assets/vector_1.png'
+
+const ProgressResolved = () => {
+    return (
+        <div className='flex justify-around mt-10'>
+            <div className='flex justify-between w-lg h-[200px] bg-gradient-to-r from-[#632ee3] to-[#9f62f2] rounded-2xl'>
+                <img className='w-50 h-50' src={vector_1} alt="" />
+                <div className='text-center text-white mt-15'>
+                    <h1 className=''>In-Progress</h1>
+                    <p className='text-4xl font-semibold'>0</p>
+                </div>
+                <img className='w-50 h-50 scale-x-[-1]' src={vector_1} alt="" />
+            </div>
+
+             <div className='flex justify-between w-lg h-[200px] bg-gradient-to-r from-[#54cf68] to-[#00827a] rounded-2xl'>
+                <img className='w-50 h-50' src={vector_1} alt="" />
+                <div className='text-center text-white mt-15'>
+                    <h1 className=''>Resolved</h1>
+                    <p className='text-4xl font-semibold'>0</p>
+                </div>
+                <img className='w-50 h-50 scale-x-[-1]' src={vector_1} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default ProgressResolved;
